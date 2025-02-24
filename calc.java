246c57e982c13f12168836fc0983ef4f9d3fff63
root <root@SELVABOOPATHI.>
2025-02-24 02:54:55 -0800

    member2

--- calc.java
+++ calc.java
@@ -1,1 +1,10 @@
+public static double multiply(double a, double b) {
+        return a * b;
+    }
 
+    public static double divide(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed.");
+        }
+        return a / b;
+    }
